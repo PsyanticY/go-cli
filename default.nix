8cb70301d925112c55fb56a226095e425552f9fc
PsyanticY <iuns@outlook.fr>
2019-09-25 13:50:03 +0100

    nix fixes

--- default.nix
+++ default.nix
@@ -2,14 +2,22 @@
 with pkgs;
 
 assert lib.versionAtLeast go.version "1.11";
+
 buildGoPackage rec {
   name = "gogo-${version}";
   version = "0.0.1";
   goPackagePath = "github.com/PsyanticY/gogo";
 
+  nativeBuildInputs = [ makeWrapper ];
+
   goDeps = ./deps.nix;
   src = ./.;
-  #allowGoReference = true;
+
+  postInstall = with stdenv; let
+    binPath = lib.makeBinPath [ nix-prefetch-git go ];
+  in ''
+    wrapProgram $bin/bin/gogo --prefix PATH : ${binPath}
+  '';
 
   meta = with stdenv.lib; {
     description = "simple go cli";
